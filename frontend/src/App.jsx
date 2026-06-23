fc2f929145f32d1491160bc9ee2513d1271cd2dd
Pranavshinde-sa <shindepranav12022@gmail.com>
2026-06-23 15:03:38 +0530

    Create home page structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,7 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="h-screen flex flex-col bg-slate-950">
-      <Navbar />
-
-      <main className="flex-1">
-        <Hero />
-      </main>
-
-      <Footer />
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
