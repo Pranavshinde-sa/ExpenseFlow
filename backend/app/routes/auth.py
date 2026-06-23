a1248d30b9574f278d5d9d88c0ae7457a68e0f2c
Pranavshinde-sa <shindepranav12022@gmail.com>
2026-06-23 22:23:49 +0530

    Implement user login verification

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -5,6 +5,7 @@ from app.schemas.user import UserCreate, UserLogin
 from app.models.user import User
 from app.core.dependencies import get_db
 from app.core.security import hash_password
+from app.core.security import verify_password
 
 router = APIRouter(
     prefix="/auth",
@@ -30,9 +31,32 @@ def signup(user: UserCreate, db: Session = Depends(get_db)):
         "id": new_user.id
     }
 
-
 @router.post("/login")
-def login(user: UserLogin):
+def login(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+
+    db_user = (
+        db.query(User)
+        .filter(User.email == user.email)
+        .first()
+    )
+
+    if not db_user:
+        return {
+            "message": "Invalid email or password"
+        }
+
+    if not verify_password(
+        user.password,
+        db_user.password
+    ):
+        return {
+            "message": "Invalid email or password"
+        }
+
     return {
-        "message": "Login successful"
+        "message": "Login successful",
+        "user_id": db_user.id
     }
