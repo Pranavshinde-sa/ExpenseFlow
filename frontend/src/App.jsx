efa8aec704b321a3d6b749082d3e19cdb3813270
Pranavshinde-sa <shindepranav12022@gmail.com>
2026-06-24 15:50:48 +0530

    Add protected routes and authentication flow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,16 +7,25 @@ import Dashboard from "./pages/dashboard";
 import Expenses from "./pages/expense";
 import Categories from "./pages/categories";
 
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Home />} />
+
         <Route path="/login" element={<Login />} />
+
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/expenses" element={<Expenses />} />
-        <Route path="/categories" element={<Categories />} />
+
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+
+        <Route path="/expenses" element={<ProtectedRoute><Expenses /></ProtectedRoute>} />
+
+        <Route path="/categories" element={<ProtectedRoute><Categories /></ProtectedRoute>} />
+
       </Routes>
     </BrowserRouter>
   );
